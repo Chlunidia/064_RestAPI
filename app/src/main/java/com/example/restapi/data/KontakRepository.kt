bbdc4ed75ce36a7e7b0969f1ac4cb2c3b7f983c3
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-12-27 18:11:55 +0700

    Create KontakRepository interface and NetworkKontakRepository class in the data package

---
+++ app/src/main/java/com/example/restapi/data/KontakRepository.kt
@@ -0,0 +1,14 @@
+package com.example.restapi.data
+
+import com.example.restapi.model.Kontak
+import com.example.restapi.network.KontakService
+
+interface KontakRepository {
+    suspend fun getKontak(): List<Kontak>
+}
+
+class NetworkKontakRepository(
+    private val kontakApiService: KontakService
+) : KontakRepository {
+    override suspend fun getKontak(): List<Kontak> = kontakApiService.getKontak()
+}
