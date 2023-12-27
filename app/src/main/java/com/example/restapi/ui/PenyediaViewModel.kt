f1bb23466598fdbca7aee37b8066dcee1dc6f088
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-12-27 22:11:36 +0700

    Create PenyediaViewModel object to use KontakRepository from KontakContainer

--- app/src/main/java/com/example/restapi/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/restapi/ui/PenyediaViewModel.kt
@@ -2,7 +2,18 @@ package com.example.restapi.ui
 
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.restapi.KontakApplication
+import com.example.restapi.ui.home.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(apkikasiKontak().container.kontakRepository)
+        }
+    }
+}
 
 fun CreationExtras.apkikasiKontak(): KontakApplication =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KontakApplication)
