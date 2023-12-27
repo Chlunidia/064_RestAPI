ee2a3c32959a64c3ee246906837361f8eb3a3b1a
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-12-27 22:26:45 +0700

    Update KontakRepository implementation for CRUD operations

--- app/src/main/java/com/example/restapi/data/KontakRepository.kt
+++ app/src/main/java/com/example/restapi/data/KontakRepository.kt
@@ -2,13 +2,48 @@ package com.example.restapi.data
 
 import com.example.restapi.model.Kontak
 import com.example.restapi.network.KontakService
+import java.io.IOException
 
 interface KontakRepository {
     suspend fun getKontak(): List<Kontak>
+    suspend fun insertKontak(kontak: Kontak)
+    suspend fun updateKontak(id: Int, kontak: Kontak)
+    suspend fun deleteKontak(id: Int)
+    suspend fun getKontakById(id: Int): Kontak
 }
 
 class NetworkKontakRepository(
     private val kontakApiService: KontakService
 ) : KontakRepository {
+    /** Fetches list of Kontak from kontakApi*/
     override suspend fun getKontak(): List<Kontak> = kontakApiService.getKontak()
+
+    // insert kontak
+    override suspend fun insertKontak(kontak: Kontak) {
+        kontakApiService.insertKontak(kontak)
+    }
+
+    // update kontak
+    override suspend fun updateKontak(id: Int, kontak: Kontak) {
+        kontakApiService.updateKontak(id, kontak)
+    }
+
+    override suspend fun deleteKontak(id: Int) {
+        try {
+            val response = kontakApiService.deleteKontak(id)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete Kontak. HTTP status code: ${response.code()}")
+            }
+            else {
+                response.message()
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    // get kontak by id
+    override suspend fun getKontakById(id: Int): Kontak {
+        return kontakApiService.getKontakById(id)
+    }
 }
